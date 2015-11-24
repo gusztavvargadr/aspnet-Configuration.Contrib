c40d761724aebfce7d0120d48d78a090be0a522c
Gusztáv Varga <me@gusztavvargadr.me>
2015-11-24 08:25:11 +0100

    Add basic sample project #16

---
+++ samples/Samples.Host/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace GV.AspNet.Configuration.ConfigurationManager.Samples.Host
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			var configurationBuilder = new ConfigurationBuilder();
+			configurationBuilder.AddAppSettings();
+			//configurationBuilder.AddAppSettings(
+			//	System.Configuration.ConfigurationManager.OpenExeConfiguration(
+			//		Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "GV.AspNet.Configuration.ConfigurationManager.Samples.Host.exe")));
+			var configuration = configurationBuilder.Build();
+
+			var value1 = configuration["Key1"];
+			var value2 = configuration["Key2"];
+		}
+	}
+}
