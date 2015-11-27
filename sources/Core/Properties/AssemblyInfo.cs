780663486404765a069fd7293dcfb1bd066e773f
Gusztáv Varga <me@gusztavvargadr.me>
2015-11-27 01:36:52 +0100

    Add description of usage of application settings #3

--- sources/Core/Properties/AssemblyInfo.cs
+++ sources/Core/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("GV.AspNet.Configuration.ConfigurationManager.Core")]
-[assembly: AssemblyDescription("System.ConfigurationManager compatibility configuration providers for the Configuration package.")]
+[assembly: AssemblyDescription("System.Configuration.ConfigurationManager compatibility configuration providers for the Configuration package.")]
 [assembly: Guid("3859914e-2549-4e08-8cce-0dc0961a8db3")]
