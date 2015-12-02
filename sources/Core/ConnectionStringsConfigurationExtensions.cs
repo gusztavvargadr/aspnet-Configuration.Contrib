2eabaced3fbadd6b4b9408d15dccca46e42b9b05
Gusztáv Varga <me@gusztavvargadr.me>
2015-12-02 01:14:59 +0100

    Add basic provider for connection strings #21

---
+++ sources/Core/ConnectionStringsConfigurationExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Configuration;
+using GV.AspNet.Configuration.ConfigurationManager;
+
+// ReSharper disable once CheckNamespace
+
+namespace Microsoft.Extensions.Configuration
+{
+	public static class ConnectionStringsConfigurationExtensions
+	{
+		public static IConfigurationBuilder AddConnectionStrings(this IConfigurationBuilder configurationBuilder)
+			=> configurationBuilder.AddConnectionStrings(ConfigurationManager.ConnectionStrings);
+
+		public static IConfigurationBuilder AddConnectionStrings(this IConfigurationBuilder configurationBuilder, System.Configuration.Configuration configuration)
+			=> configurationBuilder.AddConnectionStrings(configuration.ConnectionStrings.ConnectionStrings);
+
+		public static IConfigurationBuilder AddConnectionStrings(this IConfigurationBuilder configurationBuilder, ConnectionStringSettingsCollection connectionStrings)
+		{
+			if (configurationBuilder == null)
+			{
+				throw new ArgumentNullException(nameof(configurationBuilder));
+			}
+
+			return configurationBuilder.Add(new ConnectionStringsConfigurationProvider(connectionStrings));
+		}
+	}
+}
---
+++ sources/Core/ConnectionStringsConfigurationProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
+
+namespace GV.AspNet.Configuration.ConfigurationManager
+{
+	public class ConnectionStringsConfigurationProvider : ConfigurationProvider
+	{
+		public ConnectionStringsConfigurationProvider(ConnectionStringSettingsCollection connectionStrings)
+		{
+			if (connectionStrings == null)
+			{
+				throw new ArgumentNullException(nameof(connectionStrings));
+			}
+
+			ConnectionStrings = connectionStrings;
+		}
+
+		private ConnectionStringSettingsCollection ConnectionStrings { get; set; }
+
+		public override void Load()
+		{
+			foreach (ConnectionStringSettings connectionString in ConnectionStrings)
+			{
+				Data[$"Data:{connectionString.Name}:ConnectionString"] = connectionString.ConnectionString;
+				Data[$"Data:{connectionString.Name}:ProviderName"] = connectionString.ProviderName;
+			}
+		}
+	}
+}
